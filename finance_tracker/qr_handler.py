d75b4c4ac1f387df85f773aece87fac9a12e86ac
Radoslav Dimitrov <radddi@abv.bg>
2024-01-08 19:39:19 +0200

    Added OpenCV function to get data from QR codes

--- finance_tracker/qr_handler.py
+++ finance_tracker/qr_handler.py
@@ -7,10 +7,39 @@ Format:
 
 from dataclasses import dataclass
 import datetime as dt
+import logging
 from typing import Self
 
+import cv2 as cv
 
 QR_FORMAT = "<business_code>*<transaction_code>*<date>*<time>*<sum>"
+logger = logging.getLogger(__name__)
+
+
+def get_qr_from_video() -> str:
+    """
+    Start the default video capture device and detect a single QR code using OpenCV
+    """
+    capture = cv.VideoCapture(0)
+    detector = cv.QRCodeDetector()
+    data = None
+
+    while True:
+        try:
+            has_frame, img = capture.read()
+            if has_frame:
+                data, _, has_detection = detector.detectAndDecode(img)
+                if has_detection is not None:
+                    break
+        except KeyboardInterrupt:
+            logger.info("Received keyboard interrupt")
+            break
+        except Exception as e:
+            logger.error(e)
+            capture.release()
+            raise
+
+    return data
 
 
 @dataclass
